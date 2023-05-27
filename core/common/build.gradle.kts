281ddba7f674235641377d8d27956c88af1a2794
Adrian Czerwiński <adrczer@gmail.com>
2023-05-27 13:16:45 +0200

    Added basic navigation setup

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "pl.adrianczerwinski.prostafaktura.common"
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = AppData.kotlinCompilerExtensionVersion
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    kotlinOptions {
+        jvmTarget = AppData.jvmTarget
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        jvmTarget = AppData.jvmTarget
+    }
+}
+
+dependencies {
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.lifecycleRuntimeKtx)
+    implementation(Dependencies.activityCompose)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,8 @@ dependencyResolutionManagement {
 
 rootProject.name = "Prosta Faktura"
 include(":app")
+include(":features")
+include(":features:launch")
+include(":core")
+include(":core:ui")
+include(":core:common")
