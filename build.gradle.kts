8fef0540f5b8041ffc40430dc98edbaaa27e0828
Adrian Czerwiński <adrczer@gmail.com>
2023-08-07 18:24:16 +0200

    Upgraded gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,10 @@ import com.android.build.gradle.LibraryPlugin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.android.application") version "8.1.0-beta03" apply false
+    id("com.android.application") version "8.1.0-rc01" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
-    id("com.android.library") version "8.1.0-beta03" apply false
+    id("com.android.library") version "8.1.0-rc01" apply false
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
     id("com.google.dagger.hilt.android") version Versions.hilt apply false
     `kotlin-dsl`
